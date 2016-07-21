541c6deb77783ec565bceda3df3d918f4e122412
vmm86 <m.vasilyev86@gmail.com>
2016-07-21 16:54:56 +0300

    First commit - everything is pretty good and working

---
+++ renderer.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import jinja2
+import json
+import os
+import premailer
+import re
+
+# Исходный шаблон email-рассылки
+template = jinja2.Template(
+    open('template.j2.html', 'r').read()
+)
+
+# Пути к параметрам рассылок и сайтов, на которые они ведут
+campaigns = os.path.join(os.getcwd(), 'options', 'campaigns')
+source    = os.path.join(os.getcwd(), 'options', 'source')
+
+for opt in os.listdir(campaigns):
+    if opt.endswith('.json'):
+        # Берутся параметры конкретной рассылки
+        cur_campaign = json.load(
+            open(os.path.join(campaigns, opt), 'r')
+        )
+
+        # В зависимости от организации берутся именно её параметры
+        for src in ['vrn', 'vrn-theatre']:
+            if opt.split('_')[0] == src:
+                cur_source = json.load(
+                    open(os.path.join(source, src + '.json'), 'r')
+                )
+
+        # Слияние словарей с параметрами рассылки и её сайта
+        cur_campaign.update(cur_source)
+        # print(json.dumps(
+        #     cur_campaign, indent=4, ensure_ascii=False, sort_keys=False)
+        # )
+
+        # Отрисовка и сохранение шаблона email-рассылки
+        cur_template = template.render(cur_campaign)
+
+        # Инлайнинг стилей с помощью premailer
+        cur_template = premailer.Premailer(
+            cur_template,
+            keep_style_tags=False,
+            include_star_selectors=True,
+            capitalize_float_margin=True,
+            strip_important=False,
+            align_floating_images=False,
+            remove_unset_properties=False
+        ).transform()
+
+        # Минификация кода
+        cur_template = re.sub(r'^\ +', r'', cur_template, 0, re.MULTILINE)
+        cur_template = re.sub(r'\n',   r'', cur_template)
+
+        # Сохранение готовой подписи в HTML-файл
+        cur_output = open(
+            os.path.join(os.getcwd(), 'templates',
+                opt[:-5] + '.html'
+            ), 'w'
+        )
+        cur_output.write(cur_template)
